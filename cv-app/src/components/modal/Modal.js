62a5bb800fd96ac68df245595d75120338b5af88
Michael Traña <mtraatabladaa94@gmail.com>
2017-12-31 09:13:52 -0600

    Modificación de diseño en Components InputText, SelectDropDown

---
+++ cv-app/src/components/modal/Modal.js
@@ -0,0 +1,24 @@
+/* Node & ReactJS Modules */
+import React, { Component } from 'react';
+
+/* CSS */
+import './Modal.css';
+
+export function Modal(props) {
+
+    return(
+
+        <section className="modal modal-fixed-footer">
+            <section className="modal-content">
+                
+            </section>
+            <section className="modal-footer">
+
+            </section>
+        </section>
+
+    );
+
+}
+
+export default Modal;
