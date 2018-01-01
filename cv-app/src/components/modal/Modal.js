fe3b445f379be6d5d3e1ac973afe4f24435358b1
Michael Traña <mtraatabladaa94@gmail.com>
2018-01-01 09:49:14 -0600

    Diseño preliminar de Component Modal, ModalContent, ModalFooter

--- cv-app/src/components/modal/Modal.js
+++ cv-app/src/components/modal/Modal.js
@@ -4,21 +4,45 @@ import React, { Component } from 'react';
 /* CSS */
 import './Modal.css';
 
+/* Components */
+import { Row, Col } from '../grid/Grid';
+import $  from 'jquery';
+
 export function Modal(props) {
 
     return(
 
         <section className="modal modal-fixed-footer">
-            <section className="modal-content">
-                
-            </section>
-            <section className="modal-footer">
-
-            </section>
+            { props.children }
         </section>
 
     );
 
 }
 
-export default Modal;
+export function ModalContent(props) {
+
+    return(
+
+        <section className="modal-content">
+            { props.children }
+        </section>
+    );
+
+}
+
+export function ModalFooter(props) {
+
+    return(
+
+        <section className="modal-footer">
+            { props.children }
+        </section>
+    );
+}
+
+$(document).ready(function() {
+    $(".modal").modal();
+});
+
+export default { Modal, ModalContent, ModalFooter };
