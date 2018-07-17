8dacea95ccbf7f37b3518fc93bf3f2492e2c497b
Michael Traña <mtraatabladaa94@gmail.com>
2018-07-16 23:28:57 -0600

    Ocultando menú del AppBar en pantallas XS

--- cv-app/src/components/theme/MyAppBar.js
+++ cv-app/src/components/theme/MyAppBar.js
@@ -5,6 +5,7 @@ import { withStyles } from '@material-ui/core/styles'
 import AppBar from '@material-ui/core/AppBar'
 import Toolbar from '@material-ui/core/Toolbar'
 import Typography from '@material-ui/core/Typography'
+import Hidden from '@material-ui/core/Hidden'
 import Button from '@material-ui/core/Button'
 import IconButton from '@material-ui/core/IconButton'
 import MenuIcon from '@material-ui/icons/Menu'
@@ -91,16 +92,20 @@ export const MyAppBar = ({ classes, open, onDrawerOpen }) => (
                 />
             </Typography>
             <div className={classes.menuButtonRightSection}>
-                <Button
-                    className={classes.appBarButton}
+                <Hidden
+                    only={'xs'}
                 >
-                    Curriculum
-                </Button>
-                <Button
-                    className={classes.appBarButton}
-                >
-                    Iniciar Sesión
-                </Button>
+                    <Button
+                        className={classes.appBarButton}
+                    >
+                        Curriculum
+                    </Button>
+                    <Button
+                        className={classes.appBarButton}
+                    >
+                        Iniciar Sesión
+                    </Button>
+                </Hidden>
             </div>
         </Toolbar>
     </AppBar>
