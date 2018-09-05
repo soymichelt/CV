9c055e04b07ece6021c9891a5db0bff93cffa1d9
Michael Traña <mtraatabladaa94@gmail.com>
2018-09-05 14:31:31 -0600

    Subiendo credenciales momentaneas de Firebase

---
+++ src/firebaseCredentials.js
@@ -0,0 +1,10 @@
+export const config = {
+    apiKey: "AIzaSyCtpa-R4jkHLTsaEphnCNhdHjpULqZ4M1E",
+    authDomain: "mtraatabladaa.firebaseapp.com",
+    databaseURL: "https://mtraatabladaa.firebaseio.com",
+    projectId: "mtraatabladaa",
+    storageBucket: "mtraatabladaa.appspot.com",
+    messagingSenderId: "217863552976"
+};
+
+export default config;
