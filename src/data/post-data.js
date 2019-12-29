fb868fcf2f04961d419482e09789440e51863bf2
mtraatabladaa94@gmail.com <solanorenyer@gmail.com>
2019-12-29 16:17:10 -0600

    Funciones para agregar, editar, eliminar y listar datos de posts

--- src/data/post-data.js
+++ src/data/post-data.js
@@ -1,6 +1,34 @@
 import { getDb } from './firestore'
 
-export const getPostByIdQuery = (postId) => {
+const getPostsQuery = () => {
     const db = getDb()
-    return db.collection('posts').doc(postId)
+    return db.collection('posts')
+}
+
+export const getPostByIdQuery = (postId) => {
+    return getPostsQuery().doc(postId)
+}
+
+export const getPosts = async () => {
+    const snapshots = await getPostsQuery().get()
+    const postsArray = []
+    snapshots.forEach(post => {
+        postsArray.push({
+            id: post.id,
+            ...post.data(),
+        });
+    });
+    return postsArray
+}
+
+export const addPost = async (postData) => {
+    await getPostsQuery().add(postData)
+}
+
+export const updatePost = async (postId, postData) => {
+    await getPostByIdQuery(postId).update(postData)
+}
+
+export const deletePost = async (postId) => {
+    await getPostByIdQuery(postId).delete()
 }
